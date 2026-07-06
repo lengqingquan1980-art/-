5b085f8a867c423d297d9cfb162adad11e56c151
lengqingquan1980-art <lengqingquan1980@gmail.com>
2026-07-06 20:47:39 +0800

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,4 +1,5 @@
 import requests
+import re
 
 url = "https://www.youtube.com/@berrypassnet/videos"
 
@@ -6,11 +7,17 @@ headers = {
     "User-Agent": "Mozilla/5.0"
 }
 
-response = requests.get(url, headers=headers)
+html = requests.get(url, headers=headers).text
 
-print("Status:", response.status_code)
-print("Length:", len(response.text))
+print("网页长度：", len(html))
+
+print("\n开始寻找视频ID...\n")
+
+video_ids = re.findall(r'"videoId":"([a-zA-Z0-9_-]{11})"', html)
+
+print("找到数量：", len(video_ids))
 
 print()
-print("===== First 1000 characters =====")
-print(response.text[:1000])
+
+for vid in video_ids[:20]:
+    print(vid)
